68fb5636b2f7ffa60a9f13288d79b9d41d035d29
VaheJaloyan <vahejaloyan891@gmail.com>
2026-06-13 13:29:37 +0400

    Fixed: Api response to json.

---
+++ src/EventSubscriber/ApiExceptionSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Dto\ErrorResponseDto;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ApiExceptionSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        if (!str_starts_with($event->getRequest()->getPathInfo(), '/api/')) {
+            return;
+        }
+
+        $exception = $event->getThrowable();
+        $statusCode = $exception instanceof HttpExceptionInterface
+            ? $exception->getStatusCode()
+            : 500;
+
+        $event->setResponse(new JsonResponse(new ErrorResponseDto($exception->getMessage()), $statusCode));
+    }
+}
