a30471c1f7a171dc40cdb33f2116fd63a23927a2
VaheJaloyan <vahejaloyan891@gmail.com>
2026-06-13 11:13:34 +0400

    Updated: repositories with necessary functions.

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Dto\CustomerDataDto;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -16,28 +17,20 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    //    /**
-    //     * @return User[] Returns an array of User objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function findByEmail(string $email): ?User
+    {
+        return $this->findOneBy(['email' => $email]);
+    }
 
-    //    public function findOneBySomeField($value): ?User
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function findOrCreate(CustomerDataDto $data): User
+    {
+       $user = $this->findByEmail($data->email);
+       if ($user === null) {
+           $user = new User();
+           $user->setName($data->name);
+           $user->setEmail($data->email);
+       }
+
+       return $user;
+    }
 }
